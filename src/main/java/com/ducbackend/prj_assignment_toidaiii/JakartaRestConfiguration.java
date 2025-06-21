4b141049003132acb0c35bac08beb532c97d3352
devdwillight <ducle0554@gmail.com>
2025-06-21 10:24:29 +0700

    first commit

---
+++ src/main/java/com/ducbackend/prj_assignment_toidaiii/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.ducbackend.prj_assignment_toidaiii;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
