6c580cf423a6261b50d938d324ba0567119f9c4f
Nguyen Huu Minh Tuan <nguyenhuuminhtuan20111@gmail.com>
2025-07-15 00:02:08 +0700

    FEvnpay done

--- src/main/java/com/dao/BaseDAO.java
+++ src/main/java/com/dao/BaseDAO.java
@@ -36,7 +36,7 @@ public abstract class BaseDAO<T> {
         try {
             tx.begin();
             em.persist(entity);
-            em.flush(); // 👈 Đảm bảo entity có id sau khi persist
+            em.flush(); 
             tx.commit();
             return true;
         } catch (Exception e) {
